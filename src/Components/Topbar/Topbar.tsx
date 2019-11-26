f26602d4bcc28051b2234f79ce2a7f551233e68b
Josef Vacek <vacekj@outlook.com>
2019-11-26 16:51:39 +0100

    add icon to topbar

--- src/Components/Topbar/Topbar.tsx
+++ src/Components/Topbar/Topbar.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 import styles from "./Topbar.module.css";
 
+import logo from "../../assets/deck.svg";
+import { log } from "util";
+
 class Topbar extends React.Component<any, any> {
     public render() {
         return (
             <div className={styles.topbar}>
+                <img src={logo} alt="FlashApp Logo"/>
                 {this.props.children}
             </div>
         );
