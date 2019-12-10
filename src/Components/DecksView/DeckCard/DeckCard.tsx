c70101972c11ed70cf38e6265c3eec07be4d849f
Josef Vacek <vacekj@outlook.com>
2019-12-10 14:55:06 +0100

    better cards and score indicator

--- src/Components/DecksView/DeckCard/DeckCard.tsx
+++ src/Components/DecksView/DeckCard/DeckCard.tsx
@@ -1,20 +1,36 @@
 import React from "react";
-
+// @ts-ignore
+import ReactStoreIndicator from "react-score-indicator";
 import styles from "./DeckCard.module.css";
 import { Deck } from "../../../Lib/Storage";
-import Score from "./Score";
 
 interface Props {
-    deck: Deck,
-    accent?: boolean
+    deck: Deck;
+    accent?: boolean;
 }
 
 export default function DeckCard(props: Props) {
     return (
-        <div className={[styles.deckCard, props.accent ? styles.accent : ""].join(" ")}>
-            <span className={styles.title}>{props.deck.name}</span>
-            <div className={styles.lastSession}>{props.deck.description}</div>
-            <Score/>
+        <div
+            className={[
+                styles.deckCard,
+                props.accent ? styles.accent : ""
+            ].join(" ")}
+        >
+            <div className={styles.deckCardColumn}>
+                <span className={styles.title}>{props.deck.name}</span>
+                <div className={styles.lastSession}>
+                    {props.deck.description}
+                </div>
+            </div>
+            <div className={styles.scoreColumn}>
+                <ReactStoreIndicator
+                    value={30}
+                    maxValue={100}
+                    width={60}
+                    lineWidth={5}
+                />
+            </div>
         </div>
     );
 }
--- src/Components/DecksView/DeckCard/Score/Score.tsx
+++ src/Components/DecksView/DeckCard/Score/Score.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import ProgressLabel from "react-progress-label";
 
 import styles from "./Score.module.css";
 
@@ -8,16 +7,7 @@ class Score extends React.Component<any, any> {
         return (
             <div className={styles.score}>
                 Score:
-                <ProgressLabel
-                    progress={34}
-                    fillColor={"blue"}
-                    trackColor="#eeeeee"
-                    progressColor="#3498db"
-                    progressWidth={10}
-                    trackWidth={10}
-                    cornersWidth={5}
-                    size={30}
-                />
+
             </div>
         );
     }
