33e42dfa4f7b925c61fca146a6a66f9cb7ff22d4
Josef Vacek <vacekj@outlook.com>
2020-01-12 01:06:18 +0100

    feat: preview working

--- src/Components/AddView/Add.tsx
+++ src/Components/AddView/Add.tsx
@@ -16,11 +16,19 @@ import styles from "./Add.module.css";
 
 import NoteAddRoundedIcon from "@material-ui/icons/NoteAddRounded";
 import PlayArrowRoundedIcon from "@material-ui/icons/PlayArrowRounded";
-import { createStyles, IconButton, makeStyles } from "@material-ui/core";
+import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
+import { Dialog, IconButton, makeStyles } from "@material-ui/core";
+
+const DeckComponent = require("../Deck").default;
 
 const useStyles = makeStyles({
 	root: { color: "black" },
-	colorDisabled: { color: "grey", background: "red" }
+	colorDisabled: { color: "grey", background: "red" },
+	closeButton: {
+		position: "absolute",
+		right: 0,
+		zIndex: 30000
+	}
 });
 
 interface Props {
@@ -45,105 +53,136 @@ export default function Add(props: Props) {
 	const classes = useStyles();
 
 	return (
-		<div className={styles.addContainer}>
-			<Topbar
-				style={{
-					padding: "0 0 0 12px"
-				}}
+		<>
+			<Dialog
+				open={state.previewing}
+				onClose={() => setState({ ...state, previewing: false })}
+				fullScreen
 			>
-				<div className={styles.topbarFlex}>
-					<span>Add cards</span>
-					<div className={styles.iconsContainer}>
-						<IconButton disabled={(state.front.length + state.back.length) < 1}>
-							<PlayArrowRoundedIcon fontSize={"large"}/>
-						</IconButton>
-						<IconButton
-							disabled={state.selectedDeckId === null || state.front.length < 1 || state.back.length < 1}
-							onClick={async () => {
-								await createCard({
-									front: state.front,
-									back: state.back,
-									deckId: state.selectedDeckId as number,
-									id: Math.floor(Math.random() * 10000)
-								});
-							}}
-							classes={{
-								root: classes.root,
-								disabled: classes.colorDisabled
-							}}
-						>
-							<NoteAddRoundedIcon
-								fontSize={"large"}
-							/>
-						</IconButton>
+				<IconButton classes={{
+					root: classes.closeButton
+				}} onClick={() => setState({ ...state, previewing: false })}>
+					<CloseRoundedIcon fontSize={"large"}/>
+				</IconButton>
+				<DeckComponent
+					cards={[
+						{
+							front: state.front,
+							back: state.back,
+							id: -1,
+							deckId: -1
+						}
+					]}
+					onSwipe={() => {
+					}}
+				/>
+			</Dialog>
+			<div className={styles.addContainer}>
+				<Topbar
+					style={{
+						padding: "0 0 0 12px"
+					}}
+				>
+					<div className={styles.topbarFlex}>
+						<span>Add cards</span>
+						<div className={styles.iconsContainer}>
+							<IconButton
+								disabled={state.front.length + state.back.length < 1}
+								onClick={() => setState({ ...state, previewing: true })}
+							>
+								<PlayArrowRoundedIcon fontSize={"large"}/>
+							</IconButton>
+							<IconButton
+								disabled={
+									state.selectedDeckId === null ||
+									state.front.length < 1 ||
+									state.back.length < 1
+								}
+								onClick={async () => {
+									await createCard({
+										front: state.front,
+										back: state.back,
+										deckId: state.selectedDeckId as number,
+										id: Math.floor(Math.random() * 10000)
+									});
+								}}
+								classes={{
+									root: classes.root,
+									disabled: classes.colorDisabled
+								}}
+							>
+								<NoteAddRoundedIcon fontSize={"large"}/>
+							</IconButton>
+						</div>
 					</div>
-				</div>
-			</Topbar>
-			<div className={styles.addView}>
-				<div className={styles.deckSelectContainer}>
-					<FormControl className={styles.deckSelect}>
-						<InputLabel shrink id="demo-simple-select-label">
-							Deck
-						</InputLabel>
-						<Select
-							labelId="demo-simple-select-label"
-							id="demo-simple-select"
-							className={styles.deckSelectText}
-							displayEmpty={true}
-							autoWidth={true}
-							defaultValue={""}
-							onChange={e => {
+				</Topbar>
+				<div className={styles.addView}>
+					<div className={styles.deckSelectContainer}>
+						<FormControl className={styles.deckSelect}>
+							<InputLabel shrink id="demo-simple-select-label">
+								Deck
+							</InputLabel>
+							<Select
+								labelId="demo-simple-select-label"
+								id="demo-simple-select"
+								className={styles.deckSelectText}
+								displayEmpty={true}
+								autoWidth={true}
+								defaultValue={""}
+								onChange={e => {
+									setState({
+										...state,
+										selectedDeckId: parseInt(e.target.value as string)
+									});
+								}}
+							>
+								<MenuItem value="" disabled>
+									Select a deck
+								</MenuItem>
+								{props.decks.map((deck) => {
+									return (
+										<MenuItem value={deck.id.toString()} key={deck.id}>
+											{deck.name}
+										</MenuItem>
+									);
+								})}
+							</Select>
+						</FormControl>
+					</div>
+
+					<div className={styles.addCardContainer}>
+						<AddCard
+							placeholder={"Prompt"}
+							onChange={(text: string) => {
 								setState({
 									...state,
-									selectedDeckId: parseInt(e.target.value as string)
+									front: text
 								});
 							}}
-						>
-							<MenuItem value="" disabled>
-								Select a deck
-							</MenuItem>
-							{props.decks.map((deck, i) => {
-								return (
-									<MenuItem value={deck.id.toString()} key={deck.id}>
-										{deck.name}
-									</MenuItem>
-								);
-							})}
-						</Select>
-					</FormControl>
-				</div>
+						/>
 
-				<div className={styles.addCardContainer}>
-					<AddCard
-						placeholder={"Prompt"}
-						onChange={(text: string) => {
-							setState({
-								...state,
-								front: text
-							});
-						}}
-					/>
+						<Hidden smUp={true}>
+							<Divider
+								variant={"middle"}
+								style={{
+									width: "30%"
+								}}
+							/>
+						</Hidden>
 
-					<Hidden smUp={true}>
-						<Divider
-							variant={"middle"}
-							style={{
-								width: "30%"
+						<AddCard
+							placeholder={"Answer"}
+							onChange={(text: string) => {
+								setState({
+									...state,
+									back: text
+								});
 							}}
 						/>
-					</Hidden>
-
-					<AddCard
-						placeholder={"Answer"}
-						onChange={(text: string) => {
-							setState({
-								...state,
-								back: text
-							});
-						}}
-					/>
+					</div>
 				</div>
+				;
 			</div>
-		</div>
+		</>
 	);
 }
--- src/Components/Review/Review.tsx
+++ src/Components/Review/Review.tsx
@@ -8,73 +8,73 @@ import { Deck, getDeck, getCardsOfDeck, Card } from "../../Lib/Storage";
 const DeckComponent = require("../Deck").default;
 
 interface Props {
-    deckId: number;
+	deckId: number;
 }
 
 interface State {
-    deck: Deck;
-    cards: Card[];
+	deck: Deck;
+	cards: Card[];
 }
 
 interface onSwipeArgs {
 }
 
 export default class Review extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            deck: {
-                id: props.deckId,
-                name: "Loading",
-                description: "Loading..."
-            },
-            cards: [
-                {
-                    front: "Loading...",
-                    back: "Please wait",
-                    id: -1,
-                    deckId: -1
-                }
-            ]
-        };
-    }
+	constructor(props: Props) {
+		super(props);
+		this.state = {
+			deck: {
+				id: props.deckId,
+				name: "Loading",
+				description: "Loading..."
+			},
+			cards: [
+				{
+					front: "Loading...",
+					back: "Please wait",
+					id: -1,
+					deckId: -1
+				}
+			]
+		};
+	}
 
-    componentDidMount(): void {
-        getDeck(this.state.deck.id).then(deck => {
-            getCardsOfDeck(deck.id).then(cards => {
-                this.setState({
-                    deck,
-                    cards: cards.length ? cards : []
-                });
-            });
-        });
-    }
+	componentDidMount(): void {
+		getDeck(this.state.deck.id).then(deck => {
+			getCardsOfDeck(deck.id).then(cards => {
+				this.setState({
+					deck,
+					cards: cards.length ? cards : []
+				});
+			});
+		});
+	}
 
-    onSwipe({ index, direction, cardId }: { index: number; direction: number, cardId: number }) {
-        console.log(`Card swiped. Index: ${index} | Direction: ${direction} | cardId: ${cardId}`);
-    }
+	onSwipe({ index, direction, cardId }: { index: number; direction: number, cardId: number }) {
+		console.log(`Card swiped. Index: ${index} | Direction: ${direction} | cardId: ${cardId}`);
+	}
 
-    render() {
-        return (
-            <div
-                style={{
-                    height: "92vh"
-                }}
-            >
-                <ReviewTopbar
-                    progressIndicatorText={"15"}
-                    deckName={this.state.deck.name}
-                />
+	render() {
+		return (
+			<div
+				style={{
+					height: "92vh"
+				}}
+			>
+				<ReviewTopbar
+					progressIndicatorText={"15"}
+					deckName={this.state.deck.name}
+				/>
 
-                <div className={styles.reviewContainer}>
-                    <React.Fragment>
-                        <DeckComponent
-                            onSwipe={this.onSwipe.bind(this)}
-                            cards={this.state.cards}
-                        />
-                    </React.Fragment>
-                </div>
-            </div>
-        );
-    }
+				<div className={styles.reviewContainer}>
+					<>
+						<DeckComponent
+							onSwipe={this.onSwipe.bind(this)}
+							cards={this.state.cards}
+						/>
+					</>
+				</div>
+			</div>
+		);
+	}
 }
