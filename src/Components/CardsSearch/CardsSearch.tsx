4cbfea51f001bbf1b7fa5dd26ab6a42296cac52b
Josef Vacek <vacekj@outlook.com>
2020-02-11 19:54:05 +0100

    feat: working search

--- src/Components/CardsSearch/CardsSearch.tsx
+++ src/Components/CardsSearch/CardsSearch.tsx
@@ -8,6 +8,7 @@ import SearchRoundedIcon from "@material-ui/icons/SearchRounded";
 
 interface State {
 	cards: Card[] | null;
+	filter: string;
 }
 
 interface Props {
@@ -19,18 +20,19 @@ class CardsSearch extends React.Component<Props, State> {
 	constructor(props: Props) {
 		super(props);
 		this.state = {
-			cards: null
+			cards: null,
+			filter: ""
 		};
 	}
 
 	componentDidMount(): void {
 		this.props.decks &&
-		Promise.all(
-			this.props.decks.map(deck => this.props.storageHandler.getCardsOfDeck(deck.uid))
-		)
-			.then(arrays => arrays.flat())
-			.then(cards => this.setState({ cards }))
-			.catch(e => console.error(e));
+			Promise.all(
+				this.props.decks.map(deck => this.props.storageHandler.getCardsOfDeck(deck.uid))
+			)
+				.then(arrays => arrays.flat())
+				.then(cards => this.setState({ cards }))
+				.catch(e => console.error(e));
 	}
 
 	render() {
@@ -43,29 +45,51 @@ class CardsSearch extends React.Component<Props, State> {
 					</Link>
 				</Topbar>
 				<div className="flex flex-col justify-around">
-					<Search onSubmit={v => console.log(v)} />
+					<Search
+						onSubmit={value =>
+							this.setState({
+								filter: value
+							})
+						}
+					/>
 					{this.state.cards === null &&
-					Array(3)
-						.fill(1)
-						.map((_, key) => (
-							<div key={key}
-							     className="flex flex-col items-center justify-around rounded-lg shadow bg-white text-gray-700 p-4 m-3 mb-0 text-lg text-center">
-								<Skeleton variant="text" height={30} width={120} />
-								<Skeleton variant="text" height={30} width={120} />
-							</div>
-						))}
+						Array(3)
+							.fill(1)
+							.map((_, key) => (
+								<div
+									key={key}
+									className="flex flex-col items-center justify-around rounded-lg shadow bg-white text-gray-700 p-4 m-3 mb-0 text-lg text-center"
+								>
+									<Skeleton variant="text" height={30} width={120} />
+									<Skeleton variant="text" height={30} width={120} />
+								</div>
+							))}
 					{this.state.cards &&
-					this.state.cards.map(card => (
-						<div
-							className="flex flex-col items-center justify-around rounded-lg shadow bg-white text-gray-700 p-4 m-3 mb-0 text-lg text-center"
-							key={card.uid}
-						>
-							<div className="py-3" style={{ borderBottom: "solid 1px #cdcdcd" }}>
-								{card.front}
-							</div>
-							<div className="py-3">{card.back}</div>
-						</div>
-					))}
+						this.state.cards
+							.filter(card => {
+								if (this.state.filter == "") {
+									return true;
+								}
+
+								return (
+									card.front.includes(this.state.filter) ||
+									card.back.includes(this.state.filter)
+								);
+							})
+							.map(card => (
+								<div
+									className="flex flex-col items-center justify-around rounded-lg shadow bg-white text-gray-700 p-4 m-3 mb-0 text-lg text-center"
+									key={card.uid}
+								>
+									<div
+										className="py-3"
+										style={{ borderBottom: "solid 1px #cdcdcd" }}
+									>
+										{card.front}
+									</div>
+									<div className="py-3">{card.back}</div>
+								</div>
+							))}
 				</div>
 			</div>
 		);
@@ -88,7 +112,10 @@ function Search(props: SearchProps) {
 		>
 			<input
 				type="text"
-				onChange={e => setValue(e.target.value)}
+				onChange={e => {
+					setValue(e.target.value);
+					props.onSubmit(e.target.value);
+				}}
 				value={value}
 				className="flex-grow outline-none"
 			/>
