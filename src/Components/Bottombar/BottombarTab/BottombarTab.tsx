f08f509a48799033ddb85c02e338bccd9e5669f7
Josef Vacek <vacekj@outlook.com>
2020-02-16 12:04:08 +0100

    make bottombar tab animation faster

--- src/Components/Bottombar/BottombarTab/BottombarTab.tsx
+++ src/Components/Bottombar/BottombarTab/BottombarTab.tsx
@@ -12,14 +12,14 @@ function BottombarTab(props: Props) {
 	return (
 		<Link to={props.link} className="px-2 pt-1 flex flex-col items-center">
 			<props.icon
-				className={`transition ease-out duration-300 w-6 h-6 stroke-current fill-current ${
+				className={`transition ease-out duration-150 w-6 h-6 stroke-current fill-current ${
 					props.active ? "text-indigo-500" : "text-gray-500"
 				}`}
 			/>
 			<span
 				className={`${
 					props.active ? "text-indigo-500" : "text-gray-500"
-				} font-medium text-base transition ease-out duration-300`}
+				} font-medium text-base transition ease-out duration-150`}
 			>
 				{props.name}
 			</span>
